73867b6ed4c9a5f5f2f15059085c9dcbfac58952
KritagyaBajaj <79133250+KritagyaBajaj@users.noreply.github.com>
2022-10-04 19:57:29 +0530

    Add files via upload

---
+++ ST-86-Solution-master/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXvkUQuHxvQcMwoKESQXer0YoNU9pCX5k",
+  authDomain: "story-telling-app-e7963.firebaseapp.com",
+  databaseURL: "https://story-telling-app-e7963-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-e7963",
+  storageBucket: "story-telling-app-e7963.appspot.com",
+  messagingSenderId: "434633871936",
+  appId: "1:434633871936:web:cbb4bb923ca6a6db6085a5"
+};
